d7621824410e2b0dbcf6d904798f4f26c8a638eb
Azubuike Chijindu Okechukwu <okechukwu008@gmail.com>
2020-11-25 11:37:36 +0100

    adding animation to property card

--- Frontend/src/app/propery/property-list/property-list.component.ts
+++ Frontend/src/app/propery/property-list/property-list.component.ts
@@ -6,6 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "Abacha House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 2,
+      "Name": "Mbanefo House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 3,
+      "Name": "Eni njoku House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 4,
+      "Name": "Liberty House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 5,
+      "Name": "Kingsily House",
+      "Type": "House",
+      "Price": 12000
+    },
+    {
+      "Id": 6,
+      "Name": "Buhari House",
+      "Type": "House",
+      "Price": 12000
+    },
+  ]
 
   constructor() { }
 
